f360db44c2b440528f88a3befe8d3fb50a12c6a7
Julian Fruth <julian.fruth@datev.de>
2025-08-07 16:28:55 +0200

    Verbessere Build- und Run-Skript: füge Containerstop- und Entfernen-Befehle hinzu und optimiere die Containerstart-Optionen mit Datenbank-Volume

--- build-and-run.sh
+++ build-and-run.sh
@@ -2,7 +2,20 @@
 shopt -s expand_aliases
 alias docker='podman'
 
-docker stop monitor
-docker rm monitor
+# Aktuelles Verzeichnis für Datenbank-Volume
+CURRENT_DIR="$(pwd)"
+
+# Container stoppen und entfernen
+docker stop monitor 2>/dev/null
+docker rm monitor 2>/dev/null
+
+# Image neu bauen
 docker build -t internet-monitor .
-docker run -d --name monitor -p 8000:8000 internet-monitor
+
+# Container mit Volume für Datenbank starten
+# Die Datenbank wird im aktuellen Verzeichnis gespeichert
+docker run -d --name monitor \
+  -p 8000:8000 \
+  -v "$CURRENT_DIR:/data" \
+  -e DB_PATH="/data/data.db" \
+  internet-monitor
